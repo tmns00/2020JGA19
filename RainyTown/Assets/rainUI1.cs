9c24bf2f4842a1c53277fe178e75351439d1780c
masaatu <masaatu0416@gmail.com>
2020-04-18 02:06:52 +0900

    雨のUI作成（仮）

---
+++ RainyTown/Assets/rainUI1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class rainUI1 : MonoBehaviour
+{
+    public Image image;
+    private Sprite sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (RainStr.nextrain == 1)
+        {
+            sprite = Resources.Load<Sprite>("umbrella[0]");
+            image = this.GetComponent<Image>();
+            image.sprite = sprite;
+
+        }
+        if (RainStr.nextrain == 2)
+        {
+            sprite = Resources.Load<Sprite>("umbrella[1]");
+            image = this.GetComponent<Image>();
+            image.sprite = sprite;
+        }
+        if (RainStr.nextrain == 3)
+        {
+            sprite = Resources.Load<Sprite>("umbrella[2]");
+            image = this.GetComponent<Image>();
+            image.sprite = sprite;
+        }
+        if(RainStr.nextrain==4)
+        {
+            sprite = Resources.Load<Sprite> ("norain");
+            image = this.GetComponent<Image>();
+            image.sprite = sprite;
+        }
+    }
+}
