ea39634a13ca52af0fc8d8ccaedca3fac55c6a97
awaawa <awaawa0628@gmail.com>
2020-05-03 15:56:04 +0900

    WeaponManagerスクリプト追加

---
+++ RainyTown/Assets/ItemAsset/WeaponManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public enum WeaponType
+    {
+
+    }
+
+    [SerializeField]
+    private int attackPower;
+
+    [SerializeField]
+    private WeaponType weaponType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetAttackPower()
+    {
+        return attackPower;
+    }
+
+    public WeaponType GetWeaponType()
+    {
+        return weaponType;
+    }
+}
