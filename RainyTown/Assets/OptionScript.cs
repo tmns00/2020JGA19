e969072a4929f1820fda87fba790acee7f227e9f
awaawa <awaawa0628@gmail.com>
2020-05-09 22:36:05 +0900

    オプションの追加、一時停止機能(Zボタン)

---
+++ RainyTown/Assets/OptionScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject optionUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            optionUI.SetActive(!optionUI.activeSelf);
+
+            if(optionUI.activeSelf)
+            {
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+        }
+    }
+}
