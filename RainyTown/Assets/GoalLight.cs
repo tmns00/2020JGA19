eb7d33606f7f97c5f8b495927a66c0c0516fd2cf
ReoMatsumura <reo-hnf-pc@outlook.jp>
2020-05-12 22:20:09 +0900

    クリア時の処理を追加

---
+++ RainyTown/Assets/GoalLight.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalLight : MonoBehaviour
+{
+    public GameObject particle;
+
+    private void Update()
+    {
+        if (ItemManager.isComplete)
+            Instantiate(particle, this.transform.position, Quaternion.identity);
+    }
+}
--- RainyTown/Assets/ToBossScene.cs
+++ RainyTown/Assets/ToBossScene.cs
@@ -2,12 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ToBossScene : MonoBehaviour
 {
+    public Text clearText;
+
+    [SerializeField]
+    private float counter = 0;
+    [SerializeField]
+    private bool flag = false;
+
+    private void FixedUpdate()
+    {
+        if (flag)
+            counter += 0.05f;
+
+        if(counter >= 3)
+        {
+            clearText.text = "";
+            flag = false;
+            counter = 0;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (ItemManager.isComplete || other.gameObject.tag == "Player")
-            SceneManager.LoadScene("ProtoBoss");
+        //if (ItemManager.isComplete || other.gameObject.tag == "Player")
+        //    SceneManager.LoadScene("ProtoBoss");
+        if (ItemManager.isComplete)
+            clearText.text = "CLEAR";
+
+        if (!ItemManager.isComplete && !flag)
+        {
+            flag = true;
+            clearText.text = "鍵が足りないようだ…";         
+        }
     }
 }
