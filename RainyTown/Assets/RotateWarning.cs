7946cdf6dfbf0e5905abca803ca8090dd994c584
ReoMatsumura <reo-hnf-pc@outlook.jp>
2020-06-08 23:58:36 +0900

    強敵発生位置に警告を表示

---
+++ RainyTown/Assets/RotateWarning.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWarning : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 1, 0), 1.0f, Space.World);
+    }
+}
